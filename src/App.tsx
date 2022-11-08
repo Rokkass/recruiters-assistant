e285743497a3e0a80f87f00855cf2a180760617a
Rokkass <michal.sroka17@gmail.com>
2022-11-08 17:27:34 +0100

    Created main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Form from './pages/Form';
+import Navbar from './components/Navbar/Navbar';
+import List from './pages/List';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<List />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
